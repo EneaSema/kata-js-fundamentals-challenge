8a13fe499853ebcd50f91b54711673093e993a87
EneaSema <sema.enea1993@gmail.com>
2026-02-27 15:27:27 +0100

    resolve exercise 6

--- src/06-array-methods.js
+++ src/06-array-methods.js
@@ -3,14 +3,22 @@
 // 1. Use map to create a new array with the numbers doubled
 function doubleNumbers(arr) {
   // write your solution here
+  const double = arr.map((current) => {
+    return current * 2;
+  });
+  return double;
 }
 
 // 2. Use filter to return only even numbers
 function filterEven(arr) {
   // write your solution here
+  const even = arr.filter((a) => {
+    return a % 2 === 0;
+  });
+  return even;
 }
 
 module.exports = {
   doubleNumbers,
-  filterEven
+  filterEven,
 };
